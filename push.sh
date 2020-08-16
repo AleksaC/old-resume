ab16d5fcb64c2ba27a86ab90ac4699505a88a9f4
AleksaC <aleksacukovic1@gmail.com>
2020-08-16 02:46:23 +0200

    Update push.sh script

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 git config --global user.name "TravisCI"
 
-git add .
+git add -u
 git commit -m "Update pdf and preview image(travis build $TRAVIS_BUILD_NUMBER) [skip ci]"
-git remote set-url origin https://${GH_TOKEN}@github.com/AleksaC/resume.git > /dev/null 2>&1
+git remote set-url origin "https://${GH_TOKEN}@github.com/AleksaC/resume.git" &> /dev/null
 git push origin HEAD:master

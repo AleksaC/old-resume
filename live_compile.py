1c2e5461f1c2b7fe6020f01ff32b7c1541e5efb9
AleksaC <aleksacukovic1@gmail.com>
2021-03-30 22:46:07 +0200

    Add verbose option, use subprocess instead of os.system and run command on first run in live compile script

--- live_compile.py
+++ live_compile.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python
 
 import os
+from subprocess import Popen, DEVNULL
+import sys
 import time
 
 
 file_path = "resume.tex"
-command = "pdflatex -interaction=nonstopmode resume.tex"
+command = ["pdflatex", "-interaction=nonstopmode", "resume.tex"]
 
 
-if __name__ == '__main__':
+def run_command(command, verbose=True):
+    output = None if verbose else DEVNULL
+    process = Popen(command, stdout=output, stderr=output)
+    process.communicate()
+
+
+if __name__ == "__main__":
+    verbose = len(sys.argv) == 2 and sys.argv[1] == "--verbose"
     try:
         print(f"Watching for changes in {file_path}")
+        run_command(command, verbose=verbose)
         prev_mod = os.path.getmtime(file_path)
         while True:
             time.sleep(0.2)
             curr_mod = os.path.getmtime(file_path)
             if prev_mod != curr_mod:
-                os.system(command)
+                run_command(command, verbose=False)
                 prev_mod = curr_mod
     except KeyboardInterrupt:
-        print("\nStopping the watcher")
+        print("\nStopping the watcher...")

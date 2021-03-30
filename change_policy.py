d2aa759a4f5b9dd89c03211baf82bf0ec2386cfe
AleksaC <aleksacukovic1@gmail.com>
2021-03-30 23:10:06 +0200

    Minor fixes

--- change_policy.py
+++ change_policy.py
@@ -3,14 +3,14 @@
 import os
 from lxml import etree
 
-file_path = '/etc/ImageMagick-6/policy.xml'
+file_path = "/etc/ImageMagick-6/policy.xml"
 
-with open(file_path, 'rb+') as f:
+with open(file_path, "r+") as f:
     file_contents = f.read()
 
     root = etree.fromstring(file_contents)
     policy = root.xpath('//policy[@pattern="PDF"]')[0]
-    policy.attrib['rights'] = 'read|write'
+    policy.attrib["rights"] = "read|write"
 
     f.seek(file_contents.decode().find("<policymap>"))
     f.write(etree.tostring(root))
